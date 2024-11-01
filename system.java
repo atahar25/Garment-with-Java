fc62a77c82850543419a2aebd7fe5c37fa07aef7
Md. Atahar Molla Hira <hira23105101391@diu.edu.bd>
2024-11-01 22:48:01 +0600

    Main function implement

--- system.java
+++ system.java
@@ -124,7 +124,12 @@ class Inventory {
 }
 public class system {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Garment g1 = new Garment();
+        g1.name = "Silk";
+        g1.description = "Good Product";
+        g1.price= 600;
+        double x = g1.calculateDiscountPrice(10);
+        System.out.println(x);
     }
     
 }
